a5a26707dbd6eb97598eb55b56a60c7bee56a8e2
lappiy927 <lakkiy.game@gmail.com>
2026-05-27 13:42:45 +0900

    ドア

--- Src/Scene/GameScene.cpp
+++ Src/Scene/GameScene.cpp
@@ -227,7 +227,7 @@ void GameScene::Update(void)
 			VSub(playerPos, goalPos));
 
 		//Å@ÉhÉAÇ…ãﬂÇ√Ç¢ÇΩ
-		if (dist < 150.0f)
+		if (dist < 868.0f)
 		{
 			sceMng_.ChangeScene(
 				SceneManager::SCENE_ID::TITLE);
